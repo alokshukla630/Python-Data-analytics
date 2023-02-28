2372a46a59b185328abe2abaa2c634440450d80b
Alok Shukla <alokshukla0698@gmail.com>
2023-02-28 15:26:20 +0530

    tupple

---
+++ app_one.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.title("Demo App 😍")
+
+n1  = st.number_input('enter number')
+n2  = st.number_input('enter another number')
+c1,c2,c3,c4,c5,c6 = st.columns(6)
+add = c1.button("Add")
+sub = c2.button('Subtract')
+mul = c3.button('multiply')
+div = c4.button('Divide')
+ex  = c5.button('exponent')
+mod = c6.button('modulous')
+
+if add:
+    r = int(n1) + int(n2)
+    st.success(r)
+if sub:
+    r = int(n1) - int(n2)
+    st.success(r)
+if mul:
+    r = int(n1) * int(n2)
+    st.success(r)
+if div:
+    r = int(n1) / int(n2)
+    st.success(r)        
+if ex:
+    r = int(n1) ** int(n2)
+    st.success(r)
+if mod:
+    r = int(n1) % int(n2)
+    st.success(r)
+
+    
+    
+    
+
+
+
+
---
+++ dice.simulator.py
@@ -0,0 +1,4 @@
+# wap to create a function that will
+#  generate  random 3 dice numbers and if all 3 match,then
+#  display "you win" else display "you lose/try again"
+import random as rnd
